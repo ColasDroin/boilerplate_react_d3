39ad4af13492eb74d5e1bb19cf524369c79fddce
Colas Droin <colasdroin@gmail.com>
2025-01-21 09:53:43 +0100

    Added banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-import { data } from "@/data/data";
+import { data } from "@/../public/data/data";
 import { Barplot } from "@/components/charts/barplot/Barplot";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 export default function Home() {
   return (
-    <main className="container mx-auto px-4 py-6 " style={{ maxWidth: 600 }}>
-      <div>
+    <main>
+      <div className="w-full h-[clamp(200px,15vw,500px)] bg-gradient-to-r from-blue-500 to-purple-600 flex items-center justify-center text-white mb-8">
+        <h1 className="text-3xl md:text-5xl font-bold">
+          Welcome to the Banner
+        </h1>
+      </div>
+      <div className="container mx-auto px-4 py-6 " style={{ maxWidth: 900 }}>
         <h1>Dataviz project Boilerplate</h1>
 
         <p>
@@ -28,31 +34,7 @@ export default function Home() {
           "After all," he said, "everyone enjoys a good joke, so it's only fair
           that they should pay for the privilege."
         </blockquote>
-        <h3>The Joke Tax</h3>
-        <p>
-          The king's subjects were not amused. They grumbled and complained, but
-          the king was firm:
-        </p>
-        <ul>
-          <li>1st level of puns: 5 gold coins</li>
-          <li>2nd level of jokes: 10 gold coins</li>
-          <li>3rd level of one-liners : 20 gold coins</li>
-        </ul>
-        <p>
-          As a result, people stopped telling jokes, and the kingdom fell into a
-          gloom. But there was one person who refused to let the king's
-          foolishness get him down: a court jester named Jokester.
-        </p>
-        <Button className="mt-6">This is a button</Button>
 
-        <h3>The People's Rebellion</h3>
-        <p>
-          The people of the kingdom, feeling uplifted by the laughter, started
-          to tell jokes and puns again, and soon the entire kingdom was in on
-          the joke.
-        </p>
-
-        <p>And here is a plot</p>
         <Barplot width={500} height={400} data={data} />
       </div>
     </main>
